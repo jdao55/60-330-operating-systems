d5a7f2ff3dbaaabb5b7f50e86b265e95baa7b4ff
joedao <joedao55@gmail.com>
2018-01-22 15:56:19 -0500

    program now takes a path as second argument & added makefile

--- assignment-1/copy.c
+++ assignment-1/copy.c
@@ -3,6 +3,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
+#include <libgen.h>
+
+//returns true if path ends with '/'
+int test_path(const char* str){return (str[strlen(str) -1]=='/');}
 
 
 int main(int argc, char* argv[])
@@ -11,12 +16,18 @@ int main(int argc, char* argv[])
     if (argc !=3)
     {
         fprintf( stderr,"Incorrect number of arguments call program by:\n"
-                 "copy <src_file> <dest_file>\n");
+                 "copy <src_file> <dest_directory>\n");
         exit(1);
     }
     int src_file, dest_file;//source and destination files id's
-    char buffer[2048];//buffer for copying files
+    char buffer[2048], dest_path[512];//buffer for copying files
     int read_amt, write_amt;//return values for read() and write() calls
+
+    //append fileanme to destination path
+    strcpy(dest_path, argv[2]);
+    if (!test_path(dest_path))
+        strcat(dest_path, "/");
+    strcat(dest_path, basename(argv[1]));
 
     //open source file for read only
     src_file=open(argv[1], O_RDONLY);
@@ -27,11 +38,11 @@ int main(int argc, char* argv[])
     }
 
     //open destination file for writed only, creates a file, if file exits exit
-    dest_file=open(argv[2], O_WRONLY | O_CREAT | O_EXCL, 0777);
+    dest_file=open(dest_path, O_WRONLY | O_CREAT | O_EXCL, 0777);
     if (dest_file<0)
     {
         close(src_file);
-        fprintf(stderr,"Error opening file: %s\n", argv[2]);
+        fprintf(stderr,"Error opening file: %s\n", dest_path);
         exit(1);
     }
 
