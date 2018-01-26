5e18cf648f7182e02b0117c96e7e27d92e39920e
joedao <joedao55@gmail.com>
2018-01-25 22:00:43 -0500

    Finished Question 2 and added makefile

--- assignment-2/Q2.c
+++ assignment-2/Q2.c
@@ -1,13 +1,17 @@
 #include<sys/types.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include<sys/wait.h>
 #include<stdio.h>
 #include<string.h>
 #include<signal.h>
-void get_process_info(const char* ps_out, char * ppid, char * cmd);
 
+
+void kill_ppid(char *str);
+void show_proceses();
 int main()
 {
+    puts("Forking and running Q1\n...\n");
     pid_t pid =fork();
     if (pid <0)
     {
@@ -17,44 +21,62 @@ int main()
     //run Question 1 to create zombies
     else if (!pid)
     {
+        //Runing Q1
         execlp("./Q1", "Q1", NULL);
     }
     else
     {
         FILE *fp;
-        int status;
         char *line=NULL;
         size_t len=0;
-
-        char ppid[8], cmd[20];
-
+        
         //run ps
-        fp=popen("ps -l", "r");
+        printf("Proccess list \n");
+        show_proceses();
 
+        //kill zombies
+        printf("\nkilling zombies processes\n");
+        fp=popen("ps -l | grep defunct", "r");  
         //print output of ps, and kill any zombie processes
         while(getline(&line, &len, fp)!=-1)
-        {
-            printf("%s\n", line);
-            get_process_info(line, ppid, cmd);
-            //kill if parent if process is defunct(zombie)
-            if(strstr(cmd, "<defunct>")!=NULL)
-            {
-                kill(atoi(ppid), SIGKILL);
-            }
-        }
-        status = pclose(fp);
+            kill_ppid(line);
+        pclose(fp);
 
-
+        wait(NULL);
         //show processes after killing zombies
-        fp=popen("ps -l", "r");
-        while(getline(&line, &len, fp)!=-1)
-        {
-            printf("%s\n", line);
-        }
-        status = pclose(fp);
-
-        
-        
+        printf("\n\nUpdated proccess list \n");
+        show_proceses();
+       
     }
     return 0;
+}
+
+void kill_ppid(char *str)
+{
+    int i;
+    char * ppid, *pid;
+    strtok(str, " ");
+    for(i=0; i<2; i++)
+    {
+        strtok(NULL," ");
+    }
+    pid=strtok(NULL," ");
+    ppid=strtok(NULL," ");
+    printf("proccess %s is a zombie termimating parent process %s\n",pid, ppid);
+    kill(atoi(ppid), SIGKILL);
+}
+
+void show_proceses()
+{
+    FILE *fp;
+    char *line=NULL;
+    size_t len=0;
+    fp=popen("ps -l", "r");
+   
+    //print output of ps
+    while(getline(&line, &len, fp)!=-1)
+    {
+        printf("%s", line);
+    }
+    pclose(fp);
 }
