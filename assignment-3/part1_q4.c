00964c4307a6d5533717746b45646e266be63122
joedao <joedao55@gmail.com>
2018-02-13 19:22:38 -0500

    added part1_q4

---
+++ assignment-3/part1_q4.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+
+int circle_count=0;
+double rand_double(uint *seed){return rand_r(seed)/((double)RAND_MAX+1);}
+
+int main()
+{
+    uint seed;
+    const int args=100000000;
+    int i=0, hit_count;
+    double x,y;
+    
+
+    #pragma omp parallel private(x,y,seed, hit_count,i) shared(circle_count) num_threads(12)
+    {
+        hit_count=0;
+        seed=25234 + 17*omp_get_thread_num();
+        #pragma omp for
+        for(i=0;i<args;i++)
+        {
+            x=rand_double(&seed);
+            y=rand_double(&seed);
+            if(sqrt(x*x+y*y)<1.0)
+            { 
+                ++hit_count;
+            }
+            
+        }
+        #pragma omp critical
+        {
+            circle_count+=hit_count;
+        }
+        printf("%d\n",omp_get_thread_num());
+    }
+              
+    double pi=4.0* (circle_count/(double)args);
+    printf("Caculated PI value for %d hits out of %d points is:\n %lf\n",circle_count, args, pi);
+    
+    return 0;
+}
